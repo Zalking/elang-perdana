a0e45b2d7f013dc2bc222f991d9274cd1c77509e
Zalking <faizalramadhan.2003@gmail.com>
2025-11-01 22:22:42 +0700

    file baru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HutangRetailController;
 use App\Http\Controllers\LaporanController;
